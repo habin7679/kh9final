793d68ad7c31aa74a003f496fc86dabb8300df7d
habin7679 <habin7679@naver.com>
2022-06-27 17:16:25 +0900

    hb

--- finalproject6/src/main/java/com/kh/final6/entity/MemberDto.java
+++ finalproject6/src/main/java/com/kh/final6/entity/MemberDto.java
@@ -1,5 +1,21 @@
 package com.kh.final6.entity;
 
-public class MemberDto {
+import java.sql.Date;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class MemberDto {
+	private int memberNo;
+	private String memberId;
+	private String memberPw;
+	private String memberName;
+	private String memberNick;
+	private Date memberBirth;
+	private String memberPhone;
+	private String memberGender;
+	private String memberKind;
 }
