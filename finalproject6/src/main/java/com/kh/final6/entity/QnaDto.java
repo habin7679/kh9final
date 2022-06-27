352b47388a711aa1d86281a61f1da90414086581
ajinnoh <azin0901@naver.com>
2022-06-27 17:40:08 +0900

    DTO 추가
    - notice
    - qna
    - together
    - reply
    - attachment
    - like
    - noticeAttach

--- finalproject6/src/main/java/com/kh/final6/entity/QnaDto.java
+++ finalproject6/src/main/java/com/kh/final6/entity/QnaDto.java
@@ -1,5 +1,23 @@
 package com.kh.final6.entity;
 
-public class QnaDto {
+import java.sql.Date;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class QnaDto {
+	private long qnaNo;
+	private long memberNo;
+	private String qnaWriter;
+	private String qnaHead;
+	private String qnaTitle;
+	private String qnaContent;
+	private Date qnaTime;
+	private int qnaReadcount;
+	private int groupNo;
+	private int superNo;
+	private int depth;
 }
