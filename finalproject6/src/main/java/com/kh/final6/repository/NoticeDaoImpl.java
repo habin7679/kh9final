6ba1ee86d77e193acb56581af415ccc975988ae2
ajinnoh <azin0901@naver.com>
2022-06-27 18:21:28 +0900

    공지게시판 목록 조회 1차 테스트

--- finalproject6/src/main/java/com/kh/final6/repository/NoticeDaoImpl.java
+++ finalproject6/src/main/java/com/kh/final6/repository/NoticeDaoImpl.java
@@ -1,8 +1,22 @@
 package com.kh.final6.repository;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.kh.final6.entity.NoticeDto;
 
 @Repository
 public class NoticeDaoImpl implements NoticeDao{
+
+	@Autowired
+	private SqlSession sqlSession;
+	
+	@Override
+	public List<NoticeDto> selectList() {
+		return sqlSession.selectList("notice.list");
+	}
 
 }
