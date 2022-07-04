fb40e168e6b006545fdef308e03a9b67c26676b9
ajinnoh <azin0901@naver.com>
2022-07-04 17:25:46 +0900

    3종 게시판 디자인 구현 중, 댓글 미구현

--- finalproject6/src/main/java/com/kh/final6/controller/ReplyController.java
+++ finalproject6/src/main/java/com/kh/final6/controller/ReplyController.java
@@ -1,18 +1,36 @@
 package com.kh.final6.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kh.final6.entity.MemberDto;
+import com.kh.final6.entity.ReplyDto;
+import com.kh.final6.repository.MemberDao;
 import com.kh.final6.repository.ReplyDao;
 
 @RestController
 @RequestMapping("/rest/reply")
 public class ReplyController {
 	
-	//@Autowired
+	@Autowired
 	private ReplyDao replyDao;
 	
-	//@PostMapping("/")
+	@Autowired
+	private MemberDao memberDao;
+	
+	@PostMapping("/")
+	public ReplyDto insert(@RequestBody ReplyDto replyDto,
+						HttpSession session) {
+		int memberNo = (Integer)session.getAttribute("no");
+		String memberId = (String)session.getAttribute("login");
+		MemberDto memberDto = memberDao.info(memberId);
+		
+		replyDto.setReplyWriter(memberDto.getMemberNick());
+		return replyDao.insert(replyDto);
+	}
 }
--- finalproject6/src/main/java/com/kh/final6/repository/ReplyDaoImpl.java
+++ finalproject6/src/main/java/com/kh/final6/repository/ReplyDaoImpl.java
@@ -1,8 +1,23 @@
 package com.kh.final6.repository;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.kh.final6.entity.ReplyDto;
 
 @Repository
 public class ReplyDaoImpl implements ReplyDao{
+
+	@Autowired
+	private SqlSession sqlSession;
+	
+	@Override
+	public ReplyDto insert(ReplyDto replyDto) {
+		int replyNo = sqlSession.selectOne("reply.sequence");
+		replyDto.setReplyNo(replyNo);
+		sqlSession.insert("reply.insert",replyDto);
+		return sqlSession.selectOne("reply.one",replyNo);
+	}
 
 }
